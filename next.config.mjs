7d7f7bc630a878135e210dccb3c44197274ac504
Raj Kalakheti <kalakheti_@outlook.com>
2024-08-23 03:11:19 -0600

    github pages updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/angusbailey-portfolio' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/angusbailey-portfolio' : '',
 };
 
 export default nextConfig;
